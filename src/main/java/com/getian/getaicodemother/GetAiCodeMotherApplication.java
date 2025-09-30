045a69f1eeee72b0db8022e1d9e03518a2750aa2
get <249628024@qq.com>
2025-09-30 19:38:49 +0800

    新建项目

---
+++ src/main/java/com/getian/getaicodemother/GetAiCodeMotherApplication.java
@@ -0,0 +1,13 @@
+package com.getian.getaicodemother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetAiCodeMotherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetAiCodeMotherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/getian/getaicodemother/GetAiCodeMotherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getian.getaicodemother;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetAiCodeMotherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
