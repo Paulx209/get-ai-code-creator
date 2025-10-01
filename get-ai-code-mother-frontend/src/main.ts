9016ec6dd1a8561fb23e13c1039b080db99f72c3
get <249628024@qq.com>
2025-10-01 09:23:47 +0800

    前端初始化 （组件库->页面框架；axios请求；router路由；OpenApi生成代码）

--- get-ai-code-mother-frontend/src/main.ts
+++ get-ai-code-mother-frontend/src/main.ts
@@ -1,14 +1,15 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
 
-const app = createApp(App)
+const app = createApp(App)  //创建一个vue的应用实例，该应用实例挂在App组件上。
 
 app.use(createPinia())
 app.use(router)
+app.use(Antd)
 
 app.mount('#app')
