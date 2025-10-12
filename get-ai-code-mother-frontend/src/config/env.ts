9c6ee8b320fe86d040c030a3e4f71e69356e5c1a
get <249628024@qq.com>
2025-10-12 17:37:11 +0800

    新增’代码页面编辑‘功能

--- get-ai-code-mother-frontend/src/config/env.ts
+++ get-ai-code-mother-frontend/src/config/env.ts
@@ -10,7 +10,7 @@ export const DEPLOY_DOMAIN = import.meta.env.VITE_DEPLOY_DOMAIN || 'http://local
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8123/api'
 
 // 静态资源地址
-export const STATIC_BASE_URL = `${API_BASE_URL}/static`
+export const STATIC_BASE_URL = `${API_BASE_URL}/static`  // http://localhost:8123/api/static
 
 // 获取部署应用的完整URL
 export const getDeployUrl = (deployKey: string) => {
@@ -19,10 +19,10 @@ export const getDeployUrl = (deployKey: string) => {
 
 // 获取静态资源预览URL
 export const getStaticPreviewUrl = (codeGenType: string, appId: string) => {
-  const baseUrl = `${STATIC_BASE_URL}/${codeGenType}_${appId}/`
+  const baseUrl = `${STATIC_BASE_URL}/${codeGenType}_${appId}/`  // http://localhost:8123/api/static/multi_file_262622/
   //如果是vue项目的话
   if(codeGenType === CodeGenTypeEnum.VUE_PROJECT){
-    return `${baseUrl}dist/index.html`
+    return `${baseUrl}dist/index.html` // http://localhost:8123/api/static/vue_project_262622/dist/index.html
   }
   return baseUrl
 }
